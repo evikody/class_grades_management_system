b6eb33074f1f7894d31b3e2f8f3eda549d5b4090
unknown <evikody06@gmail.com>
2024-10-22 20:47:51 +0800

    Find the students with the highest and lowest grade

--- class_grades_management_system/class_grades_management_system.py
+++ class_grades_management_system/class_grades_management_system.py
@@ -9,20 +9,60 @@ def student_grade(grade): # Validate the grade of the student
     else:
         return False
 
-# A loop that will continue asking until a valid name is input
+class_record = []
+
 while True:
-    name = input("Please enter the student's name: ")
+    # A loop that will continue asking until a valid name is input
+    while True:
+        name = input("Please enter the student's name: ")
 
-    if student_name(name):
+        if student_name(name):
+            break
+        else:
+            print("Invalid Name. Please enter a valid name.")
+
+    # A loop that will continue asking until a valid grade is input
+    while True:
+        grade = input("Please enter the student's grade: ")
+        valid_grade = student_grade(grade)
+
+        if valid_grade:
+            break
+        else:
+            print("Invalid Grade. Please enter a valid grade.")
+
+    class_record.append({"name": name, "grade": int(grade)})
+
+    # A loop that for adding another student
+    while True:
+        another = input("Do you want to add another student? Yes or No: ").strip()
+        if another.lower() == "yes" or another.lower() == "no":
+            if another in ["Yes", "No"]:
+                break
+            else:
+                print("Invalid")
+        else:
+            print("Invalid input. Please type Yes or No.")
+
+    if another == "No":
         break
-    else:
-        print("Invalid Name. Please enter a valid name.")
 
-# A loop that will continue asking until a valid grade is input
-while True:
-    grade = input("Please enter the student's grade: ")
+if class_record:
+    top_student = class_record[0]  # Assume the first student has the highest grade
+    for student in class_record:
+        if student["grade"] > top_student["grade"]:
+            top_student = student  # Update top_student if current student has a higher grade
+    print(f"The student with the highest grade is {top_student['name']} with a grade {top_student['grade']}.")
+else:
+    print("No data collected.")
 
-    if student_grade(grade):
-        break
-    else:
-        print("Invalid Grade. Please enter a valid grade.")
+if class_record:
+    lowest_student = class_record[0]
+    for student in class_record:
+        if student["grade"] < lowest_student["grade"]:
+            lowest_student = student
+    print(f"The student with the lowest grade is {lowest_student['name']} with a grade {lowest_student['grade']}.")
+else:
+    print("No data collected.")
+
+
