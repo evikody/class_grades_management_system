5a05ab898fc883b3699cef9e4c5e26b925125e81
unknown <evikody06@gmail.com>
2024-10-22 18:46:49 +0800

    Loops that will continue asking until a valid name and grade is input

--- class_grades_management_system/class_grades_management_system.py
+++ class_grades_management_system/class_grades_management_system.py
@@ -2,8 +2,27 @@ def student_name(name): # Validates the name of the student
     return all(char.isalpha() or char in ["'", " "] for char in name) and name.strip() != ""
 
 def student_grade(grade): # Validate the grade of the student
-    if not grade.isdigit():
+    if grade.isdigit():
+        grade = int(grade)
+        if 0 <= grade <= 100:
+            return True
+    else:
         return False
 
-name = input("Please enter the student's name: ")
-grade = input("Please enter the student's grade: ")
+# A loop that will continue asking until a valid name is input
+while True:
+    name = input("Please enter the student's name: ")
+
+    if student_name(name):
+        break
+    else:
+        print("Invalid Name. Please enter a valid name.")
+
+# A loop that will continue asking until a valid grade is input
+while True:
+    grade = input("Please enter the student's grade: ")
+
+    if student_grade(grade):
+        break
+    else:
+        print("Invalid Grade. Please enter a valid grade.")
